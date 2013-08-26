5c7ee277d01fff82a451f86858b48e654386d80d
Remo Mueller <remosm@gmail.com>
2013-08-26 16:00:58 -0400

    Updated to Spout ~> 0.5.0.beta2 which includes tests to check that variables with choices have specified a domain

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -12,25 +12,10 @@ class DictionaryTest < Test::Unit::TestCase
     end
   end
 
-  def assert_domain_present(domain_name, msg = nil)
-    full_message = build_message(msg, "Variables of type choices need to specify a domain")
-    assert_block(full_message) do
-      domain_name != nil
-    end
-  end
-
   Dir.glob("variables/**/*.json").each do |file|
     if ['numeric','integer'].include?(json_value(file, :type))
       define_method("test_units: "+file) do
         assert_units json_value(file, :units)
-      end
-    end
-  end
-
-  Dir.glob("variables/**/*.json").each do |file|
-    if json_value(file, :type) == "choices"
-      define_method("test_domain_present:"+file) do
-        assert_domain_present json_value(file, :domain)
       end
     end
   end
