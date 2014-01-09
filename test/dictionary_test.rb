6cf495d29efecbd369e19257219bd7eee2eb1e47
Remo Mueller <remosm@gmail.com>
2014-01-09 13:00:40 -0500

    Removed valid units mmHG in favor of millimeters of mercury, addresses #12.

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
 
-  VALID_UNITS = ['decibels','celsius','beats per minute','millimeters of mercury','centimeters','','percent','months','years','pounds','oz','ounces','inches','feet',"kg",'kilograms','hours','days','minutes','seconds','time'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
+  VALID_UNITS = ['decibels','celsius','beats per minute','millimeters of mercury','centimeters','','percent','months','years','pounds','ounces','inches','feet',"kg",'kilograms','hours','days','minutes','seconds','time'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
 
   def assert_units(units, msg = nil)
     full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
