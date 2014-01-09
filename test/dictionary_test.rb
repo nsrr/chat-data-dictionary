483a473cf2d9a63f99a6bf8bf6f2fb33f135d6cf
Remo Mueller <remosm@gmail.com>
2014-01-09 12:56:09 -0500

    Changed units in % to percent, addresses #12.

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class DictionaryTest < Test::Unit::TestCase
   include Spout::Tests
 
-  VALID_UNITS = ['decibels','celsius','beats per minute','millimeters of mercury','centimeters','','%','percent','mmHG','months','years','lbs','pounds','oz','ounces','inches','Ft','feet',"kg",'kilograms','hours','days','minutes','seconds','time'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
+  VALID_UNITS = ['decibels','celsius','beats per minute','millimeters of mercury','centimeters','','percent','mmHG','months','years','lbs','pounds','oz','ounces','inches','Ft','feet',"kg",'kilograms','hours','days','minutes','seconds','time'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
 
   def assert_units(units, msg = nil)
     full_message = build_message(msg, "? invalid units. Valid types: #{VALID_UNITS.join(', ')}", units)
