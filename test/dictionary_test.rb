1f756b524431e8f61d30aa5fbab30da6a61aa01c
Remo Mueller <remosm@gmail.com>
2014-06-06 11:54:33 -0400

    Updated to spout 0.8.0.beta15

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,17 +1,22 @@
 require 'test_helper'
+require 'colorize'
 
 class DictionaryTest < Minitest::Test
+  # This line includes all default Spout Dictionary tests
   include Spout::Tests
+
+  # This line provides access to @variables, @forms, and @domains
+  # iterators that can be used to write custom tests
+  include Spout::Helpers::Iterators
 
   VALID_UNITS = ['seconds from date of randomization','milligrams per deciliter','micrograms per milliliter','kilograms per year','kilograms per year * meters squared','centimeters per year','','decibels','celsius','beats per minute','millimeters of mercury','centimeters','percent','months','years','pounds','ounces','inches','feet','kilograms','hours','days','minutes','seconds','events per hour','kilograms / meters squared','percentile','limb movements', 'percentage of oxygen saturation', 'desaturation events', 'limb movements per hour', 'percentage of carbon dioxide','events','millimoles per litre'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
 
-  Dir.glob("variables/**/*.json").each do |file|
-    if ['numeric','integer'].include?(json_value(file, :type))
-      define_method("test_units: "+file) do
-        units = json_value(file, :units)
-        message = "#{units} invalid units. Valid types: #{VALID_UNITS.join(', ')}"
-        assert VALID_UNITS.include?(units), message
-      end
+  @variables.select{|v| ['numeric','integer'].include?(v.type)}.each do |variable|
+    define_method("test_units: "+variable.path) do
+      message = "\"#{variable.units}\"".colorize( :red ) + " invalid units.\n" +
+                "             Valid types: " +
+                VALID_UNITS.sort.collect{|u| u.inspect.colorize( :white )}.join(', ')
+      assert VALID_UNITS.include?(variable.units), message
     end
   end
 end
