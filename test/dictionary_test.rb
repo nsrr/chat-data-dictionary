85f5b1b4373066ac4bb895652276eca83b4fb133
Michael Rueschman <michael.rueschman@gmail.com>
2017-03-29 10:49:43 -0400

    Clean up formatting of allowed units

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -12,7 +12,22 @@ class DictionaryTest < Minitest::Test
   include Spout::Helpers::Iterators
 
   # Example 1: Create custom tests to show that `integer` and `numeric` variables have a valid unit type
-  VALID_UNITS = ['natural logarithm degrees','seconds from start of recording','seconds squared','microvolts squared per hertz','hertz','standard deviations from the mean','snores','international units per milliliter','seconds from date of randomization','milligrams per deciliter','micrograms per milliliter','kilograms per year','kilograms per year * meters squared','centimeters per year','','decibels','celsius','beats per minute','millimeters of mercury','centimeters','percent','months','years','pounds','ounces','inches','feet','kilograms','hours','days','minutes','seconds','events per hour','kilograms / meters squared','percentile','limb movements', 'percentage of oxygen saturation', 'desaturation events', 'limb movements per hour', 'percentage of carbon dioxide','events','millimoles per litre'] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
+  VALID_UNITS = [
+    'natural logarithm degrees', 'seconds from start of recording',
+    'seconds squared', 'microvolts squared per hertz', 'hertz',
+    'standard deviations from the mean', 'snores',
+    'international units per milliliter', 'seconds from date of randomization',
+    'milligrams per deciliter', 'micrograms per milliliter',
+    'kilograms per year', 'kilograms per year * meters squared',
+    'centimeters per year', '', 'decibels', 'celsius', 'beats per minute',
+    'millimeters of mercury', 'centimeters', 'percent', 'months', 'years',
+    'pounds', 'ounces', 'inches', 'feet', 'kilograms', 'hours', 'days',
+    'minutes', 'seconds', 'events per hour', 'kilograms / meters squared',
+    'percentile', 'limb movements', 'percentage of oxygen saturation',
+    'desaturation events', 'limb movements per hour',
+    'percentage of carbon dioxide','events','millimoles per litre'
+  ] #Example ['mmHG','bpm','readings','minutes','%','hours','MET']
+
   @variables.select { |v| %w(numeric integer).include?(v.type) }.each do |variable|
     define_method("test_units: #{variable.path}") do
       message = "\"#{variable.units}\"".colorize(:red) + " invalid units.\n" +
